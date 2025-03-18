808bf118bc05173c96375a3a79953be28ef26e57
Lisiantys <maxime.pouzargue7@gmail.com>
2025-03-18 17:21:47 +0100

    V1.7.6.2 - debug 2

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import '../public/js/utils/clans.js';
+import '../public/js/utils/playerUtils.js';
+
+import '../public/js/classes/PlayerInfoCache.js';
+import '../public/js/classes/PreferencesManager.js';
+
+import '../public/js/classes/SimStatusListProvider.js';
+import '../public/js/translation.js';
+import '../public/js/classes/SystemListManager.js';
+import '../public/js/classes/SystemReportManager.js';
+import '../public/js/classes/SpectatorV2.js';
+
+import '../public/js/lib/mapGen.js';
+import '../public/js/lib/renderingUtils.js';
+import '../public/js/lib/ecpIcon.js';
+// // import '../public/js/lib/codemirror.min.js';
+
+import '../public/js/ui.js';
+import '../public/js/serverList.js';
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const WebpackObfuscator = require('webpack-obfuscator');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js', // Fichier principal
+    output: {
+        filename: 'bootstrap.js', // Fichier final unique
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production', // Optimisations de production (minification, tree-shaking, etc.)
+    optimization: {
+        minimize: true,
+        splitChunks: false, // Un seul fichier de sortie
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true,   // Supprime les appels à console.*
+                        drop_debugger: true,    // Supprime les instructions debugger
+                        pure_funcs: ['console.log', 'console.info', 'console.debug']
+                    },
+                    mangle: true,
+                    output: {
+                        comments: false,      // Retire tous les commentaires
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
+    plugins: [
+        new WebpackObfuscator(
+            {
+                // Obfuscation des chaînes en RC4
+                rotateStringArray: true,
+                stringArray: true,
+                stringArrayEncoding: ['rc4'],
+                stringArrayThreshold: 1, // Toutes les chaînes sont obfusquées
+
+                // Contrôle de flux très agressif (technique VM-based)
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 1,
+
+                // Protection contre le débogage
+                debugProtection: true,
+                debugProtectionInterval: 600, // Valeur en ms (>= 0)
+
+                // Désactivation de la console dans le code obfusqué
+                disableConsoleOutput: true,
+                selfDefending: true,
+
+                // Renommage global et transformation des clés d'objet
+                renameGlobals: true,
+                transformObjectKeys: true,
+
+                deadCodeInjection: false,
+            },
+            [] // Aucun fichier à exclure
+        ),
+    ],
+};
