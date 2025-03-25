ab32cfbd60bf46153ce7706dea2d424c2543cd62
Lisiantys <maxime.pouzargue7@gmail.com>
2025-03-25 16:23:45 +0100

    V1.99 - end of this project

--- render.js
+++ render.js
@@ -1,20 +1,14 @@
 const ejs = require("ejs");
 const config = require("./config.json");
 const fs = require("fs");
-const minify = require('@node-minify/core');
-const htmlMinifier = require('@node-minify/html-minifier');
+const { execSync } = require("child_process");
 
 async function main() {
-    config.revision = require('child_process').execSync('git rev-parse HEAD').toString().trim();
-    config.site.revisionTime = require('child_process').execSync('git log -1 --format=%at').toString().trim();
+    config.revision = execSync('git rev-parse HEAD').toString().trim();
+    config.site.revisionTime = execSync('git log -1 --format=%at').toString().trim();
 
-    fs.writeFileSync("./public/index.html", await ejs.renderFile("./views/index.ejs", config));
-
-    minify({
-        compressor: htmlMinifier,
-        input: "./public/index.html",
-        output: "./public/index.min.html"
-    }).then();
+    const renderedHtml = await ejs.renderFile("./views/index.ejs", config);
+    fs.writeFileSync("./public/index.html", renderedHtml);
 }
 
-main().then();
+main();
